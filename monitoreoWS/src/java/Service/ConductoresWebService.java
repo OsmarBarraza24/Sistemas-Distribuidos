7fe034a2527fa926cbbd34bdd61b6e785ea95c42
alumnog <alumnog@lgym7_maestro.itson.edu.mx>
2019-03-13 14:25:54 -0700

    Primer commit de Sistemas distribuidos

--- monitoreoWS/src/java/Service/ConductoresWebService.java
+++ monitoreoWS/src/java/Service/ConductoresWebService.java
@@ -23,7 +23,7 @@ public class ConductoresWebService {
 
     @WebMethod(operationName = "agregarConductor")
     public boolean agregarRegistro(@WebParam(name = "conductor") Object o) throws Exception {
-        Conductores conductor = (Conductores)o;
+        Conductores conductor = (Conductores) o;
         String _sql = "INSERT INTO SDEduardo.conductores (nombreCompleto, descripcion, ife, curp, rfc, licencia, vigenciaLicencia, telefono) VALUES (?,?,?,?,?,?,?,?)";
         PreparedStatement _st = this._con.prepareStatement(_sql);
         _st.setString(1, conductor.getNombreCompleto());
@@ -35,52 +35,52 @@ public class ConductoresWebService {
         _st.setString(7, conductor.getVigenciaLicencia());
         _st.setString(8, conductor.getTelefono());
         boolean resultado = _st.execute();
-        if(_st != null){
+        if (_st != null) {
             _st.close();
         }
         return resultado;
     }
-    
+
     @WebMethod(operationName = "eliminarConductor")
-    public boolean eliminarConductor(@WebParam(name = "id") int id) throws Exception{
-        String _sql = "DELETE FROM SDEduardo.conductores WHERE id="+id;
+    public boolean eliminarConductor(@WebParam(name = "id") int id) throws Exception {
+        String _sql = "DELETE FROM SDEduardo.conductores WHERE id=" + id;
         PreparedStatement _st = this._con.prepareStatement(_sql);
         boolean resultado = _st.execute();
-        if(_st != null){
+        if (_st != null) {
             _st.close();
         }
         return resultado;
     }
-    
+
     @WebMethod(operationName = "modificarConductor")
-    public boolean modificarConductor(@WebParam(name = "conductor") Object o) throws Exception{
-        Conductores conductor = (Conductores)o;
-        String _sql = "UPDATE SDEduardo.conductores SET nombreCompleto="+conductor.getNombreCompleto()+", descripcion="+conductor.getDescripcion()+", ife="+conductor.getIfe()+", curp="+conductor.getCurp()+", rfc="+conductor.getRfc()+", licencia="+conductor.getLicencia()+", vigenciaLicencia="+conductor.getVigenciaLicencia()+", telefono="+conductor.getTelefono()+" WHERE id="+conductor.getId();
+    public boolean modificarConductor(@WebParam(name = "conductor") Object o) throws Exception {
+        Conductores conductor = (Conductores) o;
+        String _sql = "UPDATE SDEduardo.conductores SET nombreCompleto=" + conductor.getNombreCompleto() + ", descripcion=" + conductor.getDescripcion() + ", ife=" + conductor.getIfe() + ", curp=" + conductor.getCurp() + ", rfc=" + conductor.getRfc() + ", licencia=" + conductor.getLicencia() + ", vigenciaLicencia=" + conductor.getVigenciaLicencia() + ", telefono=" + conductor.getTelefono() + " WHERE id=" + conductor.getId();
         PreparedStatement _st = this._con.prepareStatement(_sql);
         boolean resultado = _st.execute();
-        if(_st != null){
+        if (_st != null) {
             _st.close();
         }
         return resultado;
     }
-    
+
     @WebMethod(operationName = "consultarRegistros")
-    public Object consultarRegistros() throws Exception{
+    public Object consultarRegistros() throws Exception {
         //Conductores conductor = new Conductores();
         String _sql = "SELECT * FROM SDEduardo.conductores";
         PreparedStatement _st = this._con.prepareStatement(_sql);
-        if(_st != null){
+        if (_st != null) {
             _st.close();
         }
         ResultSet _rs = _st.executeQuery();
         return _rs;
     }
-    
+
     @WebMethod(operationName = "consultarRegistroPorId")
-    public Object consultarRegistroPorId(int id) throws Exception{
-        String _sql = "SELECT * FROM SDEduardo.conductores WHERE id="+id;
+    public Object consultarRegistroPorId(int id) throws Exception {
+        String _sql = "SELECT * FROM SDEduardo.conductores WHERE id=" + id;
         PreparedStatement _st = this._con.prepareStatement(_sql);
-        if(_st != null){
+        if (_st != null) {
             _st.close();
         }
         ResultSet _rs = _st.executeQuery();
